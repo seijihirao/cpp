e30777f5a7e555d3808c8ab8fb09405762552bb7
Tiago Tavares <tiagoft@gmail.com>
2020-07-13 17:37:45 -0300

    Demo do iostream

---
+++ 02-iostream/io.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main()
+{
+  int a;
+  float b;
+  std::cout << "Ola, mundo\n";
+  std::cin >> a;
+  std::cin >> b;
+  std::cout << a << " ---- " << b << std::endl;
+  return 0;
+}
+
