ad9022cc4c2298cca9045b90e0c9b37a20cc6c95
Rodrigo Seiji Piubeli Hirao <rodrigo@kissolutions.tech>
2021-09-01 15:57:20 -0300

    Feat: mvc on sdl.

--- sdl/tutorial.cpp
+++ sdl/tutorial.cpp
@@ -2,96 +2,194 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+using namespace std;
+
+const int SCREEN_WIDTH = 1600;
+const int SCREEN_HEIGHT = 1200;
+
+/**
+ * Background Model
+ */
+class Model {
+    private:
+        string file;
+        SDL_Rect target;
+        SDL_Texture* texture;
+
+    public:
+        Model(string file, int x, int y) : file(file) {
+            target.x = x;
+            target.y = y;
+        }
+
+        const char *getFile() {
+            return file.c_str();
+        }
+
+        SDL_Rect *getTarget() {
+            return &target;
+        }
+
+        int *getX() {
+            return &target.x;
+        }
+
+        void moveRight() {
+            target.x += 1;
+        }
+
+        void moveLeft() {
+            target.x -= 1;
+        }
+
+        int *getY() {
+            return &target.y;
+        }
+
+        void moveUp() {
+            target.y -= 1;
+        }
+
+        void moveDown() {
+            target.y += 1;
+        }
+
+        int *getW() {
+            return &target.w;
+        }
+
+        int *getH() {
+            return &target.h;
+        }
+
+        SDL_Texture* getTexture() {
+            return texture;
+        }
+
+        void setTexture(SDL_Texture *texture) {
+            this->texture = texture;
+        }
+
+        
+};
+
+/**
+ * Controller for Foreground
+ */
+class FGController {
+    private:
+        const Uint8* state;
+        const shared_ptr<Model> fg;
+    public:
+        FGController(shared_ptr<Model> fg) : fg(fg) {
+            state = SDL_GetKeyboardState(nullptr); // estado do teclado
+        }
+        
+        void move() {
+            SDL_PumpEvents(); // atualiza estado do teclado
+            if (state[SDL_SCANCODE_LEFT]) fg->moveLeft();
+            if (state[SDL_SCANCODE_RIGHT]) fg->moveRight();
+            if (state[SDL_SCANCODE_UP]) fg->moveUp();
+            if (state[SDL_SCANCODE_DOWN]) fg->moveDown();
+        }
+};
+
+/**
+ * Renderer
+ */
+class View {
+    private:
+        shared_ptr<Model> bg;
+        shared_ptr<Model> fg;
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+    public:
+        View(shared_ptr<Model> bg, shared_ptr<Model> fg) : bg(bg), fg(fg) {
+
+            // Criando uma janela
+            window = nullptr;
+            window = SDL_CreateWindow("Demonstracao do SDL2",
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                SCREEN_WIDTH,
+                SCREEN_HEIGHT,
+                SDL_WINDOW_SHOWN);
+            if (window==nullptr) { // Em caso de erro...
+                std::cout << SDL_GetError();
+                SDL_Quit();
+            }
+
+            // Inicializando o renderizador
+            renderer = SDL_CreateRenderer(
+                window, -1,
+                SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+            if (renderer==nullptr) { // Em caso de erro...
+                SDL_DestroyWindow(window);
+                std::cout << SDL_GetError();
+                SDL_Quit();
+            }
+
+            bg->setTexture(this->render(bg));
+            fg->setTexture(this->render(fg));
+        }
+
+        ~View() {
+            SDL_DestroyTexture(bg->getTexture());
+            SDL_DestroyTexture(fg->getTexture());
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+        }
+
+        SDL_Texture* render(shared_ptr<Model> model) {
+            SDL_Texture *texture = IMG_LoadTexture(renderer, model->getFile());
+            SDL_QueryTexture(texture, nullptr, nullptr, model->getW(), model->getH());
+            return texture;
+        }
+
+        void draw() {
+            // Desenhar a cena
+            SDL_RenderClear(renderer);
+            SDL_RenderCopy(renderer, bg->getTexture(), nullptr, nullptr);
+            SDL_RenderCopy(renderer, fg->getTexture(), nullptr, fg->getTarget());
+            SDL_RenderPresent(renderer);
+
+            // Delay para diminuir o framerate
+            SDL_Delay(10);
+        }
+};
 
 int main() {
-  // Inicializando o subsistema de video do SDL
-  if ( SDL_Init (SDL_INIT_VIDEO) < 0 ) {
-    std::cout << SDL_GetError();
-    return 1;
-  }
-
-  // Criando uma janela
-  SDL_Window* window = nullptr;
-  window = SDL_CreateWindow("Demonstracao do SDL2",
-      SDL_WINDOWPOS_UNDEFINED,
-      SDL_WINDOWPOS_UNDEFINED,
-      SCREEN_WIDTH,
-      SCREEN_HEIGHT,
-      SDL_WINDOW_SHOWN);
-  if (window==nullptr) { // Em caso de erro...
-    std::cout << SDL_GetError();
-    SDL_Quit();
-    return 1;
-  }
-
-  // Inicializando o renderizador
-  SDL_Renderer* renderer = SDL_CreateRenderer(
-      window, -1,
-      SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  if (renderer==nullptr) { // Em caso de erro...
-    SDL_DestroyWindow(window);
-    std::cout << SDL_GetError();
-    SDL_Quit();
-    return 1;
-  }
-
-  // Carregando texturas
-  // personagem
-  SDL_Texture *texture = IMG_LoadTexture(renderer, "./capi.png");
-  // fundo
-  SDL_Texture *texture2 = IMG_LoadTexture(renderer, "./park.jpeg");
-
-  // Quadrado onde a textura sera desenhada
-  SDL_Rect target;
-  target.x = 0;
-  target.y = 0;
-  SDL_QueryTexture(texture, nullptr, nullptr, &target.w, &target.h);
-
-  // Controlador:
-  bool rodando = true;
-
-  // Variaveis para verificar eventos
-  SDL_Event evento; // eventos discretos
-  const Uint8* state = SDL_GetKeyboardState(nullptr); // estado do teclado
-
-  // Laco principal do jogo
-  while(rodando) {
-    // Polling de eventos
-    SDL_PumpEvents(); // atualiza estado do teclado
-    if (state[SDL_SCANCODE_LEFT]) target.x -= 1;
-    if (state[SDL_SCANCODE_RIGHT]) target.x += 1;
-    if (state[SDL_SCANCODE_UP]) target.y -= 1;
-    if (state[SDL_SCANCODE_DOWN]) target.y += 1;
-
-
-    while (SDL_PollEvent(&evento)) {
-      if (evento.type == SDL_QUIT) {
-        rodando = false;
-      }
-      // Exemplos de outros eventos.
-      // Que outros eventos poderiamos ter e que sao uteis?
-      //if (evento.type == SDL_KEYDOWN) {
-      //}
-      //if (evento.type == SDL_MOUSEBUTTONDOWN) {
-      //}
+    // Inicializando o subsistema de video do SDL
+    if ( SDL_Init (SDL_INIT_VIDEO) < 0 ) {
+        std::cout << SDL_GetError();
+        return 1;
     }
 
-    // Desenhar a cena
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture2, nullptr, nullptr);
-    SDL_RenderCopy(renderer, texture, nullptr, &target);
-    SDL_RenderPresent(renderer);
+    // MVC
+    shared_ptr<Model> bg (new Model("./park.jpeg", 0, 0));
+    shared_ptr<Model> fg (new Model("./capi.png", 0, 0));
+    shared_ptr<FGController> fg_controller (new FGController(fg));
+    shared_ptr<View> view (new View(bg, fg));
 
-    // Delay para diminuir o framerate
-    SDL_Delay(10);
-  }
+    // Error handling
 
-  SDL_DestroyTexture(texture);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+    bool run = true;
 
-  return 0;
+    SDL_Event evento; // eventos discretos
+
+    // Main loop
+    while(run) {
+        fg_controller->move();
+
+        while (SDL_PollEvent(&evento)) {
+            if (evento.type == SDL_QUIT) {
+                run = false;
+            }
+        }
+
+        view->draw();
+    }
+
+    return 0;
 }
