7a1c75432cecdd7b637e59ab5009899fd84a27de
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2021-10-13 15:15:43 -0300

    Feat: exercicio_json.

--- exercicio_json/exercicio.cpp
+++ exercicio_json/exercicio.cpp
@@ -1,11 +1,16 @@
 
+#include "../json/json.hpp"
 #include <iostream>
+#include <fstream>
+
+using nlohmann::json;
 
 class Contador {
   private:
     int contagem;
 
   public:
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(Contador, contagem);
     Contador() { contagem=0; }
     void contar() { contagem++; }
     int get_contagem() { return contagem; }
@@ -13,6 +18,21 @@ class Contador {
 
 int main() {
   Contador c;
+
+  json j;
+  std::ifstream ifile;
+  ifile.open("teste.json");
+  ifile >> j;
+  ifile.close();
+
+  c = j["contador"];
+  c.contar();
+  j["contador"] = c;
+
+  std::ofstream ofile;
+  ofile.open("teste.json");
+  ofile << j;
+  ofile.close();
 
   std::cout << "Este programa executou: " << c.get_contagem() << " vezes " << std::endl;
 
